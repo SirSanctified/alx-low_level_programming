f2a7dc7bdfc48c3a77432ff5f2e7e1d1820aab53
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-02 16:14:28 -0700

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main -use puts to print to screen
+ *
+ * Description: demonstrate the use of puts.
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
