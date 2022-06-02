970f9aa6a7280ab48d9602b7a90677b831875f75
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-02 16:23:15 -0700

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main -use printf to print to screen
+ *
+ * Description: formatted printing
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
+
