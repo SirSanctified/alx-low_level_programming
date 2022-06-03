52fdcee97bec45166e404edeb8933fa0a3da4c3d
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-02 17:15:54 -0700

    Add 101-qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,11 @@
  *
  * Descriptiom: use system write
  *
- * Return: 0 on success
+ * Return: 1 on success
  */
 
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
