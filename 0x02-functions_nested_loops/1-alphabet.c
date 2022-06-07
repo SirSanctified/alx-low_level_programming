514d4dfc9dd755f11aa99509aca4717679bf7d7e
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-07 08:26:13 -0700

    Add 1-print_alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ *
+ * Description: print all letters of the alphabet using _putchar
+ */
+
+void print_alphabet(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (i > 26)
+	{
+		_putchar(alphabet[i]);
+		i++;
+	}
+	_putchar('\n');
+}
