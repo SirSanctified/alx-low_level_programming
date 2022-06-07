9eaa09def4248e998fa9e51d077e59020ee8c759
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-07 11:38:51 -0700

    Add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 lines of alphabet
+ *
+ * Description: call print_alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int = 0; i < 10; i++)
+		print_alphabet();
+}
