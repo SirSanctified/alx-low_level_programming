ee4c420feddfc356809f47e7bd2742670ceab33f
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-07 13:17:48 -0700

    Add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - find absolute value of a number
+ * @num: number to be checked
+ *
+ * Description: find absolute
+ *
+ * Return: absolute of num
+ */
+
+int _abs(int num)
+{
+	return (num < 0 ? -num : num);
+}
