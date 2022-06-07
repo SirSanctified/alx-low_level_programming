8b87ea3d4d7042c41ecb1a7a76ce2cd58a6b9032
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-07 07:58:26 -0700

    Add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Description: use the putchar function in main.h
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char str[8] = "_putchar";
+	while (i < 8)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	return 0;
+}
