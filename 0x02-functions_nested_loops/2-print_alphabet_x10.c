ba1e8607f0e61ab551c86832bf1620783653f1ce
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-07 12:16:14 -0700

    Fix 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,16 +10,17 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
-	int j = 0;
 
 	while (i < 10)
 	{
+		int j = 0;
+
 		while (j < 26)
 		{
 			_putchar(alpha[j]);
 			j++;
 		}
 		_putchar('\n');
-		i++;;
+		i++;
 	}
 }
