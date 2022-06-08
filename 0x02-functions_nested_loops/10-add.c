66dd1b08fd8a9453c6af405234dd7c5ab4227e5a
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-07 18:49:51 -0700

    Add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add 2 integers
+ * @a: first number
+ * @b: second number
+ *
+ * Description: find sum of 2 numbers
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isalpha(int c);
 int _abs(int);
 int print_last_digit(int);
 void times_table(void);
+int add(int, int);
