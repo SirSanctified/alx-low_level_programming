87c694ec6e4fe2c56900c5bd413c1d8fdfd494fa
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-07 17:56:51 -0700

    Add 7_print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a given number
+ * @num: the given number
+ *
+ * Description: calculate using modulo 10
+ *
+ * Return: the last digit
+ */
+
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
