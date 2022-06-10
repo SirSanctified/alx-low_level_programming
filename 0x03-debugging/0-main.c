5017d99d3c9d0956ceea5ef02ac644d9a57bd188
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-10 03:54:18 -0700

    Add 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests positivity or negativity of a number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
