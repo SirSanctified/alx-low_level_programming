460bc5b99767297bfe427a41fdd9f45e6561341d
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-13 09:21:56 -0700

    Add 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - reset an integer to 98
- * @*n: the pointer to a variable to be reset
+ * @n: the pointer to a variable to be reset
  *
  * Description: reset a number pointed to by a pointer to 98
  */
