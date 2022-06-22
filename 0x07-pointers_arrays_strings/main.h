ef1f9ad3be1919bd153ca5ad15b8468261eca05f
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-21 17:53:44 -0700

    Add 4-strpbrk.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int( char *, char *);
+char *_strpbrk(char *, char *);
