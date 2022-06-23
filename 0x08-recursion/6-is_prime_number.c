8cefccba09053a18fd50a81cf15107a27b7d2ee9
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-22 17:16:41 -0700

    Add 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check if a number is prime
+ * @n: the number to check
+ * Return: 1 if true else 0
+ */
+
+int is_prime_number(int n)
+{
+	int i = n / 2;
+
+	if (i == 1)
+	{
+		return (1);
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		i = i - 1;
+		is_prime_number(n);
+	}
+}
