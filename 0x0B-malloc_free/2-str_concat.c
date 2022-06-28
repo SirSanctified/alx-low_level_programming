be8e36a3ed004c7135baa368aa4883f6c8a88fde
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-28 08:51:26 -0700

    Add 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,13 +18,13 @@ char *str_concat(char *s1, char *s2)
 	if (result == NULL)
 		return (NULL);
 
-	if (s1 == NULL && s2 != NULL)
+	if (*s1 == NULL && *s2 != NULL)
 		return (s2);
 
-	if (s1 != NULL && s2 == NULL)
+	if (*s1 != NULL && *s2 == NULL)
 		return (s1);
 
-	if (s1 == NULL && s2 == NULL)
+	if (*s1 == NULL && *s2 == NULL)
 		return (NULL);
 
 	temp = result;
