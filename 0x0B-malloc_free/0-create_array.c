9fffd172dd5f89a412abe1d847641ecc4a43c29c
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-06-28 07:10:48 -0700

    Fix 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return ('\0');
 
-	if (!(arr = malloc(sizeof(char) * size)))
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
