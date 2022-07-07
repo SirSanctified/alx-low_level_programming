4a41567bb3a86a96aa0309c753e77c930e586620
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-07-07 07:41:58 -0700

    Add 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdarg.h>
+
+/**
+ * sum_them_all - sum up all variable number of arguments
+ * @n: the number of passed argument
+ * Return: sum of all variables on success or 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int i = 0;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	while (i < n)
+	{
+		sum += va_arg(args, int);
+		i++;
+	}
+	return (sum);
+}
