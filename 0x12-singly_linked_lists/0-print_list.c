13211678c62e30b5cf2f5b7d26c574d509ad1ffb
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-07-14 07:03:10 -0700

    Add 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		if (temp->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", temp->len, temp->str);
+			printf("[%u] %s\n", temp->len, temp->str);
 		
 		temp = temp->next;
 		count++;
