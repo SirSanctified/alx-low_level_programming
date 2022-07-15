c50755ffdbe2cc2682796616b186b2d83d0de786
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-07-14 19:33:41 -0700

    Add 4-free_list.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * my_func - execute before main
+ * Return: nothing
+ */
+
+void my_func(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
